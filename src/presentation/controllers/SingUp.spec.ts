547d0b80b3fded0d1ac1ba88f8ee8bb9ce87ac2e
gervasioartur <gervasioartur@outlook.com.br>
2022-06-01 09:08:55 -0300

    test: ensure SingUoControler calls EmailValidator with correct email

--- src/presentation/controllers/SingUp.spec.ts
+++ src/presentation/controllers/SingUp.spec.ts
@@ -96,4 +96,19 @@ describe('Sing up controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new InvalidParamError('email'))
     })
+
+    it('should call EmailValidator with correct email', () => {
+        const { sut, emailValidatorStub } = makeSut()
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                email: 'any_email@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        sut.handle(httpRequest)
+        expect(isValidSpy).toHaveBeenLastCalledWith('any_email@email.com')
+    })
 })
