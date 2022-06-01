f3677c28f3ec32165dcb4441f0c2d290212241c5
gervasioartur <gervasioartur@outlook.com.br>
2022-05-31 21:28:12 -0300

    test: ensure SingUpCOntroller returns 400 if no email is provided

--- src/presentation/controllers/SingUp.spec.ts
+++ src/presentation/controllers/SingUp.spec.ts
@@ -3,15 +3,29 @@ import { SingUpController } from './SingUp'
 describe('Sing up controller', () => {
     test('should retun 400 if no name is provided', () => {
         const sut = new SingUpController()
-        const httpResquest = {
+        const httpRequest = {
             body: {
                 email: 'any_email@email.com',
                 password: 'any_password',
                 passwordConfirmations: 'any_password'
             }
         }
-        const httpResponse = sut.handle(httpResquest)
+        const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new Error('Missing param : name'))
+    })
+
+    test('should retun 400 if no name is provided', () => {
+        const sut = new SingUpController()
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                password: 'any_password',
+                passwordConfirmations: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param : email'))
     })
 })
