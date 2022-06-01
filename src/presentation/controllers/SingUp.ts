85b2bc1442aea6f48c141362beea819c700d860a
gervasioartur <gervasioartur@outlook.com.br>
2022-05-31 22:13:11 -0300

    refactor: make SingUp conforms to a procol

--- src/presentation/controllers/SingUp.ts
+++ src/presentation/controllers/SingUp.ts
@@ -1,7 +1,8 @@
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
-export class SingUpController {
+export class SingUpController implements Controller{
     handle(httpRequest: HttpRequest): HttpResponse {
         const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
         for(const field of requiredFields){
