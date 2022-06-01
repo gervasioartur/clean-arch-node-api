bd79b15c5b7ce10c812b07dc50a8419b0cedfb21
gervasioartur <gervasioartur@outlook.com.br>
2022-06-01 17:47:27 -0300

    refactor:  move sut creation to facory helper method

--- src/data/useCases/add-account/db-add-acount.spec.ts
+++ src/data/useCases/add-account/db-add-acount.spec.ts
@@ -1,16 +1,27 @@
+import { Encrypter } from "@/data/protocol/encrypter"
 import { DbAddAccount } from "./db-add-acount"
+interface SuTypes {
+    sut: DbAddAccount
+    encryptStub: Encrypter
+}
+const makeSut = (): SuTypes => {
+    class EncryptStub {
+        async encrypt(value: string): Promise<string> {
+            return new Promise(resolve => resolve('hasehed_password'))
+        }
+    }
 
+    const encryptStub = new EncryptStub()
+    const sut = new DbAddAccount(encryptStub)
+    return {
+        sut,
+        encryptStub
+    }
+}
 describe('DbAddAccount UseCase', () => {
     it('should call Encrypter with correct password', async () => {
-        class EncryptStub {
-            async encrypt(value: string): Promise<string> {
-                return new Promise(resolve => resolve('hasehed_password'))
-            }
-        }
-
-        const encryptStub = new EncryptStub()
-        const sut = new DbAddAccount(encryptStub)
-        const encryptSpy = jest.spyOn(encryptStub, 'encrypt')
+        const { sut, encryptStub } = makeSut()
+         const encryptSpy = jest.spyOn(encryptStub, 'encrypt')
         const accountData = {
             name: 'valid_name',
             email: 'valid_emai',
