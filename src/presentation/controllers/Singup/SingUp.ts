ecbcb01d134d0f3b5ddb91cb7b2254fa87c879c0
gervasioartur <gervasioartur@outlook.com.br>
2022-06-01 11:37:20 -0300

    refactor: move success http response to a helper method

--- src/presentation/controllers/Singup/SingUp.ts
+++ src/presentation/controllers/Singup/SingUp.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpRequest, HttpResponse, EmailValidator, AddAccount } from './singup-protocols'
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, serverError, ok } from '../../helpers/http-helper'
 
 export class SingUpController implements Controller {
     constructor(
@@ -32,10 +32,7 @@ export class SingUpController implements Controller {
                 email,
                 password
             })
-            return {
-                statusCode: 200,
-                body: account
-            }
+            return ok(account)
         } catch (error) {
             return serverError(error)
         }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -10,3 +10,8 @@ export const serverError = (error: Error): HttpResponse => ({
     statusCode: 500,
     body: new ServerError()
 })
+
+export const ok = (data: any): HttpResponse => ({
+    statusCode: 200,
+    body: data
+})
