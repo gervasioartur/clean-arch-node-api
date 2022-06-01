03a4e8bf468bbf71b2f35d6a711e3cfdab4d2180
gervasioartur <gervasioartur@outlook.com.br>
2022-05-31 22:02:18 -0300

    feat: ensure SingUpControler return 400 if no password is provider

--- src/presentation/controllers/SingUp.ts
+++ src/presentation/controllers/SingUp.ts
@@ -3,7 +3,7 @@ import { badRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 export class SingUpController {
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = ['name', 'email']
+        const requiredFields = ['name', 'email', 'password']
         for(const field of requiredFields){
             if(!httpRequest.body[field]){
                 return badRequest(new MissingParamError(field))
