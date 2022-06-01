57c4373a96a1800c1f33d232d962ee493edbf284
gervasioartur <gervasioartur@outlook.com.br>
2022-05-31 21:10:09 -0300

    feat: ensure SingUpCOntroller returns 400 if no email is provided

--- src/presentation/controllers/SingUp.ts
+++ src/presentation/controllers/SingUp.ts
@@ -1,8 +1,17 @@
 export class SingUpController {
-    handle(httpResquest: any): any {
-        return {
-            statusCode: 400,
-            body: new Error('Missing param : name')
+    handle (httpResquest: any): any {
+        if (!httpResquest.body.name) {
+            return {
+                statusCode: 400,
+                body: new Error('Missing param : name')
+            }
+        }
+
+        if (!httpResquest.body.email) {
+            return {
+                statusCode: 400,
+                body: new Error('Missing param : email')
+            }
         }
     }
 }
