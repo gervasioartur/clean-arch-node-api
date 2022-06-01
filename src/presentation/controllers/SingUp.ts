9c43f05b08dba43e21d83308f1adacd810340585
gervasioartur <gervasioartur@outlook.com.br>
2022-06-01 09:36:34 -0300

    refactor: move all protocol imports to a single file

--- src/presentation/controllers/SingUp.ts
+++ src/presentation/controllers/SingUp.ts
@@ -1,8 +1,7 @@
 import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helpers/http-helper'
-import { Controller } from '../protocols/controller'
-import { EmailValidator } from '../protocols/email-validator'
-import { HttpRequest, HttpResponse } from '../protocols/http'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols'
+
 export class SingUpController implements Controller {
     constructor(private readonly emailValidator: EmailValidator) {
         this.emailValidator = emailValidator
