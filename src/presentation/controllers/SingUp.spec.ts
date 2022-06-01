8e7d2d6cbfb9f6d1da731d1f5d3fa2c38f56918b
gervasioartur <gervasioartur@outlook.com.br>
2022-06-01 10:09:54 -0300

    test: ensure SingUpController returns 400 if password confirmations fails

--- src/presentation/controllers/SingUp.spec.ts
+++ src/presentation/controllers/SingUp.spec.ts
@@ -91,6 +91,22 @@ describe('Sing up controller', () => {
         expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
     })
 
+    it('should retun 400 if no password confirmation is fails', () => {
+        const { sut } = makeSut()
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                email: 'any_email@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'invalid_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+    })
+
+
     it('should retun 400 if the email invalid', () => {
         const { sut, emailValidatorStub } = makeSut()
         jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
