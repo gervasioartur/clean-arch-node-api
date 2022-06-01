be6223e3ec4f2f7e7c8843940b4cbb2ec3b09a05
gervasioartur <gervasioartur@outlook.com.br>
2022-06-01 13:50:29 -0300

    chore:  add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,9 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
+  },
 }
