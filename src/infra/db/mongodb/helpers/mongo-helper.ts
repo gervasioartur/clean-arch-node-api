d06e10c8bb0d7cc80e96d2c8021050c86b7053ed
bluvalida <bluvalida@outlook.com>
2022-06-03 14:35:53 -0300

    chore: add config to mongodb memory serve

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,7 +2,6 @@ import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
     client: null as MongoClient,
-
     async connect (uri: string): Promise<void> {
         this.client = await MongoClient.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
