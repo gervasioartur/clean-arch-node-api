666724d4f655676d4e5abe319ccd8c85db332fa1
bluvalida <bluvalida@outlook.com>
2022-06-03 14:03:06 -0300

    feat: ensure AccountMongoRepository returns an accoutn on succss

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,14 +1,19 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
     client: null as MongoClient,
+
     async connect (uri: string): Promise<void> {
-        this.client = await MongoClient.connect(globalThis.__MONGO_URI__, {
+        this.client = await MongoClient.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
     },
     async disconnect (): Promise<void> {
         await this.client.close()
+    },
+
+    getCollection (name: string): Collection {
+        return this.client.db().collection(name)
     }
 }
