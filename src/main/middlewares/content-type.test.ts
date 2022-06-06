621a7adcf9850b2bdc784f723c0fb22f76a549e1
gervasioartur <gervasioartur@outlook.com.br>
2022-06-06 13:40:43 -0300

    chore: ensure return xml content type if forced

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -10,4 +10,14 @@ describe('Contet-type middleaware', () => {
             .get('/test_content_type')
            .expect('content-type', /json/)
     })
+
+    it('shoud return xml content type when forced',async () => {
+        app.get('/test_content_type_xml', (req, res) => {
+            res.type('xml')
+            res.send('')
+        })
+        await request(app)
+            .get('/test_content_type_xml')
+           .expect('content-type', /xml/)
+    })
 })
