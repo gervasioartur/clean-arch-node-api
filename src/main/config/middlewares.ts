635af6a112fb2e8b68d07192bd133cbc778b099d
gervasioartur <gervasioartur@outlook.com.br>
2022-06-06 13:37:12 -0300

    feat: add set default contet  type as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import {Express}  from 'express'
 import { cors } from '../middlewares/cors'
 import { bodyParser } from '../middlewares/body-parser'
+import { contentType } from '../middlewares/content-type'
 
 export default (app: Express):  void => {
+    app.use(contentType)
     app.use(bodyParser)
     app.use(cors)
 }  
