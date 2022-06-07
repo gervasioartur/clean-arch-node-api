7ea19f050e2d9618969a2f0baaa4156edaeac677
gervasioartur <gervasioartur@outlook.com.br>
2022-06-07 15:42:12 -0300

    feat: add Singup Factory

---
+++ src/main/factories/singup.ts
@@ -0,0 +1,14 @@
+import { DbAddAccount } from "@/data/useCases/add-account/db-add-acount";
+import { BcryptAdapter } from "@/infra/criptography/bcrypt-adapter";
+import { AccountMongoRepository } from "@/infra/db/mongodb/account-repository/account";
+import {SingUpController } from "@/presentation/controllers/sing-up/sing-up";
+import { EmailValidatorAdapter } from "@/utils/email-validator-adapter";
+
+export const makeSingUpControler = () : SingUpController => {
+    const salt = 12
+    const emailValidatorAdapter = new EmailValidatorAdapter()
+    const bcryptAdpter =  new BcryptAdapter(12)
+    const accountMongoRepository = new AccountMongoRepository()
+    const dbAddAccount = new DbAddAccount(bcryptAdpter, accountMongoRepository)
+    return new SingUpController(emailValidatorAdapter, dbAddAccount)
+}
