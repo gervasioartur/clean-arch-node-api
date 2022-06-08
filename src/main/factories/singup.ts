e81d695f54915335a5cdfc9aa20fe2d4b5c6001e
gervasioartur <gervasioartur@outlook.com.br>
2022-06-08 13:58:48 -0300

    feat: ensure log controller decorator calls controller with correct value

--- src/main/factories/singup.ts
+++ src/main/factories/singup.ts
@@ -1,14 +1,18 @@
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols';
 import { DbAddAccount } from '../../data/useCases/add-account/db-add-acount'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
-import {SingUpController } from "../../presentation/controllers/sing-up/sing-up"
+import { SingUpController } from "../../presentation/controllers/sing-up/sing-up"
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+import { LogControllerDecorator } from '../decorators/log';
 
-export const makeSingUpControler = () : SingUpController => {
+export const makeSingUpController = (): Controller => {
     const salt = 12
     const emailValidatorAdapter = new EmailValidatorAdapter()
-    const bcryptAdpter =  new BcryptAdapter(12)
+    const bcryptAdpter = new BcryptAdapter(12)
     const accountMongoRepository = new AccountMongoRepository()
     const dbAddAccount = new DbAddAccount(bcryptAdpter, accountMongoRepository)
-    return new SingUpController(emailValidatorAdapter, dbAddAccount)
+    const singUpController = new SingUpController(emailValidatorAdapter, dbAddAccount)
+
+    return new LogControllerDecorator(singUpController)
 }
--- src/main/routes/singup.routes.ts
+++ src/main/routes/singup.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import {makeSingUpControler} from '../factories/singup'
+import {makeSingUpController} from '../factories/singup'
 import { adaptRoute } from "../adapters/express-routes-adapter";
 
-export default  (router: Router): void => {
-    router.post('/singup', adaptRoute(makeSingUpControler()))
+export default (router: Router): void => {
+    router.post('/singup', adaptRoute(makeSingUpController()))
 }
