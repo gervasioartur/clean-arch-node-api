d74772fd533fd01b52edd5f7ff4a993c115fefbe
gervasioartur <gervasioartur@outlook.com.br>
2022-06-14 14:45:34 -0300

    feat: ensure  loginController returns 200 , if valid credentials is provider

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from "../../../presentation/errors";
-import { badRequest, serverError, unauthorized } from "../../../presentation/helpers/http-helper";
+import { badRequest, ok, serverError, unauthorized } from "../../../presentation/helpers/http-helper";
 import { Controller, HttpRequest, HttpResponse, EmailValidator, Authentication } from "./login-protocols";
 
 export class LoginController implements Controller {
@@ -28,6 +28,7 @@ export class LoginController implements Controller {
        if (!accessToken) {
         return unauthorized()
        }
+       return ok({ accessToken })
      } catch (error) {
         return serverError(error)
      }
