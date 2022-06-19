6c9ddd27d52088f0f947288356c6ac6570fc1228
gervasioartur <gervasioartur@outlook.com.br>
2022-06-19 12:38:29 -0300

    feat: add loginFactory

--- src/main/routes/login.routes.ts
+++ src/main/routes/login.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { makeSingUpController } from '../factories/singup/singup'
+import { makeSingUpController } from '../factories/singup/singup-factory'
 import { adaptRoute } from "../adapters/express/express-routes-adapter";
 
 export default (router: Router): void => {
