12aae40a6d2100f5c3b9ab336055a9129fad216f
gervasioartur <gervasioartur@outlook.com.br>
2022-06-20 14:19:49 -0300

    chore: enable debug with vs code

--- src/main/routes/login.routes.ts
+++ src/main/routes/login.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { adaptRoute } from "../adapters/express/express-routes-adapter";
 import { makeLoginController } from "../factories/login/login-factory";
-import { makeSingUpController } from '../factories/singup/singup-factory'
+import { makeSingUpController } from "../factories/singup/singup-factory"
 
 export default (router: Router): void => {
     router.post('/singup', adaptRoute(makeSingUpController()))
