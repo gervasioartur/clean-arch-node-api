91e584e4563cba8f792cfbd478ecd6bda4daa02f
gervasioartur <gervasioartur@outlook.com.br>
2022-06-20 22:43:53 -0300

    feat: ensure singupController returns accessToken when sucess

--- src/presentation/controllers/sing-up/singup-controller.spec.ts
+++ src/presentation/controllers/sing-up/singup-controller.spec.ts
@@ -1,4 +1,4 @@
-import { AddAccount, AccountModel, AddAccountModel, LoadAccountById, HttpRequest,Validation } from './singup-controller-protocols'
+import { AddAccount, AccountModel, AddAccountModel, Authentication, AuthenticationModel,LoadAccountById, HttpRequest,Validation } from './singup-controller-protocols'
 import { SingUpController } from './singup-controller'
 import { MissingParamError, InvalidParamError, ServerError } from '../../errors'
 import { ok, badRequest } from '../../helpers/http/http-helper'
@@ -21,15 +21,6 @@ const makeaddAccount = (): AddAccount => {
     return new AddAccountStub()
 }
 
-const makeLoadAccountById = (): LoadAccountById => {
-    class LoadAccountByIdStub implements LoadAccountById {
-        async load (accountId: any): Promise<AccountModel> {
-            return new Promise(resolve => resolve(makefakeAccount()))
-        }
-    }
-    return new LoadAccountByIdStub()
-}
-
 const makeValidation = (): Validation => {
     class ValidationStub implements Validation {
         validate (input: any): Error | null {
@@ -38,82 +29,43 @@ const makeValidation = (): Validation => {
     }
     return new ValidationStub()
 }
+
+const makeAuthentication = (): Authentication => {
+    class AuthenticatioStub implements Authentication {
+        async auth (Authentication: AuthenticationModel): Promise<string> {
+            return new Promise(resolve => resolve('any_token'))
+        }
+    }
+    return new AuthenticatioStub()
+}
 interface SutTypes {
     sut: SingUpController
     addAccountStub: AddAccount
     validationStub: Validation
-    loadAccountByIdStub: LoadAccountById
+    authenticationStub: Authentication
 }
 
 const makefakeAccount = (): AccountModel => ({
     id: 'valid_id',
     name: 'valid_name',
-    email: 'valid_email',
+    email: 'valid_email@email.com',
     password: 'valid_password'
 })
 
 const makeSut = (): SutTypes => {
     const addAccountStub = makeaddAccount()
     const validationStub = makeValidation()
-    const loadAccountByIdStub = makeLoadAccountById()
-    const sut = new SingUpController(addAccountStub,validationStub, loadAccountByIdStub)
+    const authenticationStub = makeAuthentication()
+    const sut = new SingUpController(addAccountStub,validationStub, authenticationStub)
     return {
         sut,
         addAccountStub,
         validationStub,
-        loadAccountByIdStub
+        authenticationStub
     }
 }
 
 describe('Sing up controller', () => {
-    it('should call add account with correct values', async () => {
-        const { sut, addAccountStub } = makeSut()
-        const addSpy = jest.spyOn(addAccountStub, 'add')
-        const httpResponse = await sut.handle(makeFakeRequest())
-        expect(addSpy).toHaveBeenLastCalledWith({
-            name: "any_name",
-            email: 'any_email@email.com',
-            password: 'any_password'
-        })
-    })
-    
-    it('should call  LoadByEmail with correct values', async () => {
-        const { sut, loadAccountByIdStub } = makeSut()
-        const loadByIsSpy = jest.spyOn(loadAccountByIdStub, 'load')
-        await loadAccountByIdStub.load('any_accout_id')
-        expect(loadByIsSpy).toHaveBeenLastCalledWith('any_accout_id')
-    })
-
-    it('should return 401 if LoadByEmail returns null', async () => {
-        const { sut, loadAccountByIdStub } = makeSut()
-        jest.spyOn(loadAccountByIdStub, 'load').mockReturnValueOnce(null)
-        const httpResponse = await sut.handle(makeFakeRequest())
-        const account = await loadAccountByIdStub.load('any_accout_id')
-        expect(httpResponse.statusCode).toBe(401)
-    })
-
-    it('should return an account on success', async () => {
-        const { sut, loadAccountByIdStub } = makeSut()
-        const account = await loadAccountByIdStub.load('any_accout_id')
-        expect(account).toEqual(makefakeAccount())
-    })
-
-    it('should retun 500 if  addAcount throws', async () => {
-        const { sut, addAccountStub } = makeSut()
-        jest.spyOn(addAccountStub, 'add').mockImplementationOnce(async () => {
-            return new Promise((resolve, reject) => reject(new Error()))
-        })
-        const httpResponse = await sut.handle(makeFakeRequest())
-        expect(httpResponse.statusCode).toBe(500)
-        expect(httpResponse.body).toEqual(new ServerError(''))
-    })
-
-    it('should retun 200 if valid data is provided', async () => {
-        const { sut } = makeSut()
-        const httpResponse = await sut.handle(makeFakeRequest())
-        expect(httpResponse).toEqual(ok(makefakeAccount()))
-    })
-
     it('should call Validation with correct value', async () => {
         const { sut, validationStub } = makeSut()
         const validateSpy = jest.spyOn(validationStub, 'validate')
@@ -127,5 +79,52 @@ describe('Sing up controller', () => {
         jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
         const httpResponse = await sut.handle(makeFakeRequest())
         expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+    })
+
+    it('should call add account with correct values', async () => {
+        const { sut, addAccountStub } = makeSut()
+        const addSpy = jest.spyOn(addAccountStub, 'add')
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(addSpy).toHaveBeenLastCalledWith({
+            name: "any_name",
+            email: 'any_email@email.com',
+            password: 'any_password'
+        })
+    })
+
+    it('should retun 500 if  addAcount throws', async () => {
+        const { sut, addAccountStub } = makeSut()
+        jest.spyOn(addAccountStub, 'add').mockImplementationOnce(async () => {
+            return new Promise((resolve, reject) => reject(new Error()))
+        })
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse.statusCode).toBe(500)
+        expect(httpResponse.body).toEqual(new ServerError(''))
+    })
+
+    it('Should  call authentication with correct values', async () => {
+        const { sut, authenticationStub } = makeSut()
+        const authSpy = jest.spyOn(authenticationStub, 'auth')
+        await sut.handle(makeFakeRequest())
+        expect(authSpy).toHaveBeenCalledWith({
+            email: 'valid_email@email.com',
+            password: 'valid_password'
+        })
+    })
+
+    it('Should  retur 500 if authentication thows', async () => {
+        const { sut, authenticationStub } = makeSut()
+        jest.spyOn(authenticationStub, 'auth').mockImplementationOnce(async () => {
+            return new Promise((resolve, reject) => reject(new Error()))
+        })
+       const httpResponse = await sut.handle(makeFakeRequest())
+       expect(httpResponse.statusCode).toBe(500)
+    })
+
+    it('Should statusCode 200 and accessToken on success', async () => {
+        const { sut, authenticationStub } = makeSut()
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse.statusCode).toBe(200)
+        expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
     })
 })
