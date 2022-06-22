17818223599e231a8f8646a65e620f29d133d0a6
gervasioartur <gervasioartur@outlook.com.br>
2022-06-22 14:43:51 -0300

    feat: ensure AuthMiddleware return 403 if no x-access-token existis in headers

---
+++ src/presentation/middlewares/auth-middleware.ts
@@ -0,0 +1,10 @@
+import { AccessDenied } from "../errors";
+import { forbidden } from "../helpers/http/http-helper";
+import { HttpRequest, HttpResponse, Middleware } from "../protocols";
+
+export class AuthMiddleware implements Middleware {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+        const error = forbidden(new AccessDenied())
+        return new Promise(resolve => resolve(error))
+    }
+}
