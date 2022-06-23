841768abc7e4b5b89a0dace44544451cb85c6345
gervasioartur <gervasioartur@outlook.com.br>
2022-06-23 09:01:49 -0300

    feat: ensure DbLoadAccountByToken call Decrypter with correct values

---
+++ src/data/useCases/load-account-by-token/db-load-account-by-token.ts
@@ -0,0 +1,12 @@
+import { LoadAccountByToken, Decrypter, AccountModel } from './db-load-account-protocols'
+
+export class DbLoadAccountBytoken implements LoadAccountByToken {
+    constructor ( 
+        private readonly decrypter: Decrypter
+    ) { }
+
+    async load (accessToken: string, role?: string): Promise<AccountModel> {
+        await this.decrypter.decrypt(accessToken)
+        return new Promise(resolve => resolve(null))
+    }   
+}
---
+++ src/data/useCases/load-account-by-token/db-load-account-protocols.ts
@@ -0,0 +1,3 @@
+export * from "../../protocols/criptography/decrypter";
+export * from "../../../domain/useCases/load-account-by-token";
+export * from "../add-account/db-add-account-protocols";
