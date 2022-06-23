ad43323797064a76f532b9465e11cba958dd6f82
gervasioartur <gervasioartur@outlook.com.br>
2022-06-23 09:38:43 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

--- src/data/useCases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/useCases/load-account-by-token/db-load-account-by-token.ts
@@ -1,12 +1,16 @@
-import { LoadAccountByToken, Decrypter, AccountModel } from './db-load-account-protocols'
+import { LoadAccountByToken, Decrypter, AccountModel, LoadAccountByTokenRepository } from './db-load-account-protocols'
 
 export class DbLoadAccountBytoken implements LoadAccountByToken {
     constructor ( 
-        private readonly decrypter: Decrypter
+        private readonly decrypter: Decrypter,
+        private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
     ) { }
 
     async load (accessToken: string, role?: string): Promise<AccountModel> {
-            await this.decrypter.decrypt(accessToken)
+        const token = await this.decrypter.decrypt(accessToken)
+        if (token) {
+            await this.loadAccountByTokenRepository.loadByToken(token, role)
+        }
         return null
     }   
 }
--- src/data/useCases/load-account-by-token/db-load-account-protocols.ts
+++ src/data/useCases/load-account-by-token/db-load-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from "../../protocols/criptography/decrypter";
 export * from "../../../domain/useCases/load-account-by-token";
 export * from "../add-account/db-add-account-protocols";
+export * from '../../protocols/db/account/load-account-by-token-repository'
