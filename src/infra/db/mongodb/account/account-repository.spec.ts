8a09311b7dc9523caa5801b62414557110a92d7f
gervasioartur <gervasioartur@outlook.com.br>
2022-06-23 11:39:09 -0300

    test: ensure AcountMongoRepository return an acount if loadByToken returns an account without role

--- src/infra/db/mongodb/account/account-repository.spec.ts
+++ src/infra/db/mongodb/account/account-repository.spec.ts
@@ -57,6 +57,30 @@ describe('Account Mongo repo', () => {
         })
     })
 
+    describe('loadByToken()', () => {
+        it('Shoud return an account on loadByToken without role success', async () => {
+            const sut = makeSut()
+            await accountsColletion.insertOne({
+                name: 'any_name',
+                email: 'any_email@email.com',
+                password: 'any_password',
+                accessToken: 'any_token'
+            })
+            const account = await sut.loadByToken('any_token')
+            expect(account).toBeTruthy()
+            expect(account.id).toBeTruthy()
+            expect(account.name).toBe('any_name')
+            expect(account.email).toBe('any_email@email.com')
+            expect(account.password).toBe('any_password')
+        })
+
+        // it('Shoud return null if loadByEmail fails', async () => {
+        //     const sut = makeSut()
+        //     const account = await sut.loadByEmail('any_email@email.com')
+        //     expect(account).toBeFalsy()
+        // })
+    })
+
     describe('loadById()', () => {
         it('Shoud return an account on loadById success', async () => {
             const sut = makeSut()
