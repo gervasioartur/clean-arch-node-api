0dadaf9c119fb5151933b3aece6d69bec21f1dd1
gervasioartur <gervasioartur@outlook.com.br>
2022-06-24 09:50:56 -0300

    chore: add heroku deploy config to travis v.2

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 
 docker login --username $HEROKU_DOCKER_USERNAME --password $HEROKU_API_KEY registry.heroku.com
-docker-compose up
 heroku container:push -a clean-node-api-v web
 heroku container:release -a clean-node-api-v web
