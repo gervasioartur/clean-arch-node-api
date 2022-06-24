2a035adc6bbb790d8263b2de8bf175ef7e880bf3
gervasioartur <gervasioartur@outlook.com.br>
2022-06-24 10:23:19 -0300

    chore: preparing for deploy v.5

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 
-docker login --username $HEROKU_DOCKER_USERNAME --password $HEROKU_API_KEY registry.heroku.com
-yarn up
 docker tag clean-node-api-v registry.heroku.com/clean-node-api-v
 heroku container:push -a clean-node-api-v web
 heroku container:release -a clean-node-api-v web
