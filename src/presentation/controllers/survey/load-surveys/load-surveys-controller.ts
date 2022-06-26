f9ef291eea40ede2c427c87dfa0e13463b38300b
gervasioartur <gervasioartur@outlook.com.br>
2022-06-26 15:32:40 -0300

    test: ensure LoadSurveysController retuns status 500 if LoadSurveys throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,13 +1,17 @@
 import { Controller, HttpRequest, HttpResponse } from "../../../protocols";
 import { LoadSurveys } from '../../../../domain/useCases/load-survey'
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 
 export class LoadSurveysController implements Controller {
     constructor (
         private readonly loadSurvey: LoadSurveys
-    ) {}
+    ) { }
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const surveys = await this.loadSurvey.load()
-        return ok(surveys)
+        try {
+            const surveys = await this.loadSurvey.load()
+            return ok(surveys)
+        } catch (error) {
+            return serverError(error)
+        }
     }
 }
