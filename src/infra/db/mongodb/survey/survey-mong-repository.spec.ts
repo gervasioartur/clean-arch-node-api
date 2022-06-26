ff6dd50843e060c316c58da40710922c579ba05a
gervasioartur <gervasioartur@outlook.com.br>
2022-06-26 18:03:35 -0300

    test: ensure LoadAll retuns a list of Surveys on sucess

--- src/infra/db/mongodb/survey/survey-mong-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mong-repository.spec.ts
@@ -42,24 +42,43 @@ describe('Survey Mongo repo', () => {
         })
     })
 
-    describe('LaodAll', () => {
-        it('should add asurvey on success', async () => {
+    describe('LaodAll()', () => {
+        it('should load survey on success', async () => {
+            await surveyColletion.insertMany([
+                {
+                    question: 'any_question',
+                    answers: [
+                        {
+                            image: 'any_image',
+                            answer: 'any_answer'
+                        },
+                        {
+                            answer: 'any_answer_2'
+                        }
+                    ],
+                    date: new Date()
+                },
+
+                {
+                    question: 'any_question_1',
+                    answers: [
+                        {
+                            image: 'any_image',
+                            answer: 'any_answer'
+                        },
+                        {
+                            answer: 'any_answer_2'
+                        }
+                    ],
+                    date: new Date()
+                }
+            ])
+
             const sut = makeSut()
-            await sut.add({
-                question: 'any_question',
-                answers: [
-                    {
-                        image: 'any_image',
-                        answer: 'any_answer'
-                    },
-                    {
-                        answer: 'any_answer_2'
-                    }
-                ],
-                date: new Date()
-            })
-            const survey = await surveyColletion.findOne({ question: 'any_question' })
-            expect(survey).toBeTruthy()
+            const surveys = await sut.loadAll()
+            expect(surveys.length).toBe(2)
+            expect(surveys[0].question).toBe('any_question')
+            expect(surveys[1].question).toBe('any_question_1')
         })
     })
 })
