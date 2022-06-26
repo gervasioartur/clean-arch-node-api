2530885145b1b162eee0ee6bde2774ee3280332b
gervasioartur <gervasioartur@outlook.com.br>
2022-06-26 15:32:19 -0300

    feat: ensure LoadSurveysController retuns status 500 if LoadSurveys throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,7 +1,7 @@
 import { LoadSurveysController } from './load-surveys-controller'
 import { SurveyModel } from '../../../../domain/models/survey'
 import { LoadSurveys } from '../../../../domain/useCases/load-survey'
-import { ok } from '../../../helpers/http/http-helper'
+import { ok , serverError } from '../../../helpers/http/http-helper'
 import Mockdate from 'mockdate'
 
 const makeFakeSurveys = (): SurveyModel[] => {
@@ -59,5 +59,14 @@ describe('', () => {
         const { sut, loadSurveysStub } = makeSut()
         const httpResponse = await sut.handle({})
         expect(httpResponse).toEqual(ok(makeFakeSurveys()))
+    })
+
+    it('Should return 500 if AddSurvey throws', async () => {
+        const { sut, loadSurveysStub } = makeSut()
+        const addSpy = jest.spyOn(loadSurveysStub, 'load').mockImplementationOnce(async () => {
+            return new Promise((resolve, reject) => reject(new Error()))
+        })
+        const httpResponse = await sut.handle({})
+        expect(httpResponse).toEqual(serverError(new Error()))
     })
 })  
