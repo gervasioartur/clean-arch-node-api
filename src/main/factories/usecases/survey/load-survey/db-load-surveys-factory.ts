0fea0824eda9f028edf8dc582b3ebd074bf5a0b3
gervasioartur <gervasioartur@outlook.com.br>
2022-06-27 14:48:25 -0300

    fix: fix module import

--- src/main/factories/usecases/survey/load-survey/db-load-surveys-factory.ts
+++ src/main/factories/usecases/survey/load-survey/db-load-surveys-factory.ts
@@ -1,6 +1,6 @@
 import { DbLoadSurveys } from '@/data/useCases/load-surveys/db-load-surveys'
 import { SurveyMongoRepository } from '@/infra/db/mongodb/survey/survey-mongo-repository'
-import { LoadSurveys } from '@/domain/useCases/load-survey'
+import { LoadSurveys } from '@/domain/useCases/load-surveys'
 
 export const makeDbLoadSurvey = (): LoadSurveys => {
     const surveyMongoRepository = new SurveyMongoRepository()
