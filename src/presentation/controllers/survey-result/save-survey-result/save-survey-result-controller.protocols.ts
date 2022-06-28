eb997784bbab4641841080755c3411c768366490
gervasioartur <gervasioartur@outlook.com.br>
2022-06-28 13:27:54 -0300

    feat: ensure SaveResultConroller returns 403 if LoadSurveyById retuns null

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.protocols.ts
@@ -1,3 +1,5 @@
 export * from "@/presentation/protocols/index";
 export * from "@/domain/useCases/survey/load-surveys-by-id"
 export * from "@/domain/models/survey"
+export * from "@/presentation/errors"
+export * from "@/presentation/helpers/http/http-helper"
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,4 +1,4 @@
-import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from "./save-survey-result-controller.protocols"
+import { Controller, HttpRequest, HttpResponse, LoadSurveyById, forbidden, InvalidParamError } from "./save-survey-result-controller.protocols"
 
 export class SaveSurveyResultController implements Controller {
     constructor (
@@ -6,7 +6,10 @@ export class SaveSurveyResultController implements Controller {
     ) {}
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
         const { surveyId } = httpRequest.params
-        await this.loadSurveyById.loadById(surveyId)
-        return new Promise(resolve => resolve(null))
+        const surveyById = await this.loadSurveyById.loadById(surveyId)
+        if (!surveyById) {
+            return forbidden(new InvalidParamError('surveyId'))
+        }
+        return null
     }
 }
