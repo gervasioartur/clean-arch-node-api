0f65cb1dbc655fac356ab4c306c07599976e6331
gervasioartur <gervasioartur@outlook.com.br>
2022-06-28 13:41:43 -0300

    feat: ensure SaveSurveyResultController return a server error if LoadById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -1,5 +1,5 @@
 import { SaveSurveyResultController } from "./save-survey-result-controller"
-import { HttpRequest, LoadSurveyById, SurveyModel, forbidden, InvalidParamError } from "./save-survey-result-controller.protocols"
+import { HttpRequest, LoadSurveyById, SurveyModel, forbidden, InvalidParamError, serverError } from "./save-survey-result-controller.protocols"
 import Mockdate from 'mockdate'
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -64,5 +64,14 @@ describe('SaveSurveyResultController', () => {
         jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(new Promise(resolve => resolve(null)))
         const httpResponse = await sut.handle(makeFakeRequest())
         expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
+    })
+
+    it('Should return 500 if LoadSurveyById throws', async () => {
+        const { sut, loadSurveyByIdStub } = makeSut()
+        jest.spyOn(loadSurveyByIdStub, 'loadById').mockImplementationOnce(async () => {
+            return new Promise((resolve, reject) => reject(new Error()))
+        })
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse).toEqual(serverError(new Error()))
     })
 })
